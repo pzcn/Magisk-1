91935298a0298aec6be5f61d52380e82617bb49a
pzcn <sdustpedro@gmail.com>
2022-03-04 16:41:43 +0800

    Update HideAPK.kt

--- app/src/main/java/com/topjohnwu/magisk/core/tasks/HideAPK.kt
+++ app/src/main/java/com/topjohnwu/magisk/core/tasks/HideAPK.kt
@@ -118,7 +118,7 @@ object HideAPK {
 
         // Generate a new random package name and signature
         val repack = File(activity.cacheDir, "patched.apk")
-        val pkg = genPackageName()
+        val pkg = "mag.isk"
         Config.keyStoreRaw = ""
 
         if (!patch(activity, stub, FileOutputStream(repack), pkg, label))
